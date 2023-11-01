1aacfed38ab342d8c4efd18787115c68eddc3e7f
rimgosu <newnyup@gmail.com>
2023-11-01 10:08:18 +0900

    Wed Nov  1 10:08:18     2023

---
+++ cassandra.zip_expanded/cassandra/src/test/java/kr/spring/main/CassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.spring.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
